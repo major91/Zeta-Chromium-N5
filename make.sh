5c7b424e4b9801883259a04b43dd22c12424e432
Yuanhan Liu <yuanhan.liu@linux.intel.com>
2013-02-01 18:59:16 +0800

    miss

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #make kernel tool
-export PATH=$(pwd)/linaro_4.8/bin:$PATH
+export PATH=$(pwd)/linaro_14.02/bin:$PATH
 export ARCH=arm 
 export SUBARCH=arm
 export CROSS_COMPILE=/home/major/kernel/linaro_14.02/bin/arm-linux-gnueabihf-
