a902a6873033861cb2d05875f9d80ffa6a4add7d
arter97 <qkrwngud825@gmail.com>
2014-02-20 17:50:03 +0900

    sdcardfs: re-port it from SHV-E330K_KK_Opensource_Update3.zip

--- fs/sdcardfs/dentry.c
+++ fs/sdcardfs/dentry.c
@@ -179,9 +179,3 @@ const struct dentry_operations sdcardfs_ci_dops = {
 	.d_hash 	= sdcardfs_hash_ci,
 	.d_compare	= sdcardfs_cmp_ci,
 };
-
-const struct dentry_operations sdcardfs_dops = {
-	.d_revalidate	= sdcardfs_d_revalidate,
-	.d_release	= sdcardfs_d_release,
-};
-
