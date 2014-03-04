30e1bf312e73f25e3c99ab98ec365685f98eeadb
major91 <jacksn91@naver.com>
2014-03-04 21:35:09 +0900

    drivers: touch_synaptics: Add PWR_KEY suspend to touch driver and cle…
    …anup
    thanks to neobuddy89

--- drivers/input/touchscreen/touch_synaptics_ds5.c
+++ drivers/input/touchscreen/touch_synaptics_ds5.c
@@ -1676,7 +1676,7 @@ static int lcd_notifier_callback(struct notifier_block *this,
 	struct synaptics_ts_data *ts =
 		container_of(this, struct synaptics_ts_data, notif);
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-#if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE) || defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
+#if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE) || defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) || defined(CONFIG_PWRKEY_SUSPEND)
 	bool prevent_sleep = false;
 #endif
 #if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE)
@@ -1685,10 +1685,10 @@ static int lcd_notifier_callback(struct notifier_block *this,
 #if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
 	prevent_sleep = prevent_sleep || (dt2w_switch > 0);
 #endif
-#endif
 #ifdef CONFIG_PWRKEY_SUSPEND
 	if (pwrkey_pressed)
 		prevent_sleep = false;
+#endif
 #endif
 
 	TOUCH_DEBUG_TRACE("%s: event = %lu\n", __func__, event);
@@ -1866,10 +1866,14 @@ static int synaptics_ts_probe(
 	gpio_direction_input(ts->pdata->irq_gpio);
 
 	ret = request_threaded_irq(client->irq, NULL, touch_irq_handler,
-#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+#if defined(CONFIG_TOUCHSCREEN_PREVENT_SLEEP)
+#if defined(CONFIG_PWRKEY_SUSPEND)
 			IRQF_TRIGGER_FALLING | IRQF_ONESHOT, client->name, ts);
 #else
-			IRQF_TRIGGER_FALLING | IRQF_ONESHOT, client->name, ts);
+		        IRQF_TRIGGER_FALLING | IRQF_ONESHOT | IRQF_NO_SUSPEND, client->name, ts);
+#endif
+#else
+                        IRQF_TRIGGER_FALLING | IRQF_ONESHOT, client->name, ts);
 #endif
 
 	if (ret < 0) {
