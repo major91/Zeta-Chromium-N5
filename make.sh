0468b2b091214365c436a3ebaa35f0845690a0de
major91 <jacksn91@naver.com>
2014-03-28 21:09:25 +0900

    toolchain: upgrade toolchain path

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #make kernel tool
-export PATH=$(pwd)/linaro_14.02/bin:$PATH
+export PATH=$(pwd)/linaro_14.03/bin:$PATH
 export ARCH=arm 
 export SUBARCH=arm
-export CROSS_COMPILE=/home/major/kernel/linaro_14.02/bin/arm-linux-gnueabihf-
+export CROSS_COMPILE=/home/major/kernel/linaro_14.03/bin/arm-linux-gnueabihf-
 make ZC_defconfig
 make -j4
