33cfc8c140d518c5686b119005826cd9d1e30568
major91 <jacksn91@naver.com>
2014-05-02 21:55:34 +0900

    defconfig update & change toolchain

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #make kernel tool
-export PATH=$(pwd)/linaro_4.10/bin:$PATH
+export PATH=$(pwd)/linaro_4.8/bin:$PATH
 export ARCH=arm 
 export SUBARCH=arm
-export CROSS_COMPILE=/home/major/kernel/linaro_4.10/bin/arm-eabi-
+export CROSS_COMPILE=/home/major/kernel/linaro_4.8/bin/arm-linux-gnueabihf-
 make ZC_defconfig
 make -j4
