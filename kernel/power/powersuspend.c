fcdbcc6438294a30ec71295a5b0a2e2fe8ba5230
franciscofranco <franciscofranco.1990@gmail.com>
2014-05-09 12:15:06 +0000

    Revert cpuidle: move field disable from per_driver to per_cpu

--- kernel/power/powersuspend.c
+++ kernel/power/powersuspend.c
@@ -276,8 +276,7 @@ static struct attribute_group power_suspend_attr_group =
 
 static struct kobject *power_suspend_kobj;
 
-// ------------------------------------------ sysfs interface ------------------------------------------
-
+// ------------------ sysfs interface -----------------------
 static int __init power_suspend_init(void)
 {
 
