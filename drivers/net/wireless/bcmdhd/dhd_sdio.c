ccc1154e242d3a143d169dcfbc512bddba8ebe14
arter97 <qkrwngud825@gmail.com>
2014-06-05 19:37:47 +0900

    Revert "bcmdhd: fix redefined symbol"
    This reverts commit 2a2aecb38ec5fdc23ae62af7e013c962a6492fc1.

--- drivers/net/wireless/bcmdhd/dhd_sdio.c
+++ drivers/net/wireless/bcmdhd/dhd_sdio.c
@@ -1498,7 +1498,7 @@ dhd_enable_oob_intr(struct dhd_bus *bus, bool enable)
 #endif
 
 #ifdef DHDTCPACK_SUPPRESS
-uint dhd_use_tcpack_suppress;
+extern bool dhd_use_tcpack_suppress;
 
 /* Please be sure this function is called under dhd_os_tcpacklock() */
 void dhd_onoff_tcpack_sup(void *pub, bool on)
