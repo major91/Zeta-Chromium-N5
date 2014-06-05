8dd9d27ecdf52cb684714898044a1007765cb0f0
franciscofranco <franciscofranco.1990@gmail.com>
2014-06-05 00:46:23 +0000

    msm: mdss_fb: with interruptible thread we'll have problems rebooting…
    …, for example adb reboot bootloader the framebuffer deadlocks. Fix it by using it as CAF designed in the first place.

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1615,15 +1615,14 @@ static int __mdss_fb_display_thread(void *data)
 		pr_info("%s: set priority failed\n", __func__);
 
 	while (1) {
-		wait_event_interruptible(mfd->commit_wait_q,
+		wait_event(mfd->commit_wait_q,
 				(atomic_read(&mfd->commits_pending) ||
-				 kthread_should_stop()));
+				kthread_should_stop()));
 
 		if (kthread_should_stop())
 			break;
- 
-		ret = __mdss_fb_perform_commit(mfd);
 
+		ret = __mdss_fb_perform_commit(mfd);
 		atomic_dec(&mfd->commits_pending);
 		wake_up_all(&mfd->idle_wait_q);
 	}
