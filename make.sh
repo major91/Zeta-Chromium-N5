13b09ca81da75c473f52352ad3d344cdf7cac350
major91 <jacksn91@naver.com>
2014-07-23 14:01:40 +0900

    update toolchain linaro_4.9 (07)

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@
 export PATH=$(pwd)/android-toolchain-eabi/bin:$PATH
 export ARCH=arm 
 export SUBARCH=arm
-export CROSS_COMPILE=/home/major/kernel/android-toolchain-eabi/bin/arm-eabi-
+export CROSS_COMPILE=/home/major/kernel/android-toolchain-eabi/bin/arm-linux-androideabi-
 make ZC_defconfig
 make -j4
